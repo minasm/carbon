c672c43161939fde14f77025c6cee6a5ac8703d0
minasm <minas@miroglu.com>
2023-03-17 12:15:12 +0000

    Add formatting

--- src/Carbon.php
+++ src/Carbon.php
@@ -13,8 +13,13 @@
             $interval = new DateInterval('P1D');
             $dateRange = new DatePeriod($startDate, $interval, $endDate->add($interval)); 
             $holidays = array_keys((new Carbon())->getBankHolidays([new Carbon($startDate),new Carbon($endDate)]));
-            
+
+            $holidays = array_map( function($item) use( $format){
+                return DateTime::createFromFormat( "Y-m-d", $item)->format( $format);
+            },$holidays);
+
             $businessDays = 0;
+            
             foreach ($dateRange as $date) {                
                 if($date->format('N') < 6 && !in_array($date->format($format),$holidays)) {
                     ++$businessDays;
